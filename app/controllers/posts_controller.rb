0477c60823ded78a3aa1beeddf31d370590c1b0e
zaziden <n.n1013n.n@icloud.com>
2021-07-16 18:02:12 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   #end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
